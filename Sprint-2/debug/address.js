bf46e353751db26c5858b84dd21f29a570d13cef
Trang <dothuytrang2312@gmail.com>
2026-01-16 23:12:33 +0100

    Complete debug/address.js

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -5,11 +5,18 @@
 // Fix anything that isn't working
 
 const address = {
-  houseNumber: 42,
+  houseNumber: "42",
   street: "Imaginary Road",
   city: "Manchester",
   country: "England",
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address["houseNumber"]}`);
+
+// Prediction: 
+// I think it should be ';' instead of ',' at the end of each line
+
+// It was wrong. Actually we should put houseNumber in the ""
+
+// It was not enough. In fact, because address is object, it the [] we cannot use the index but the name of the key like "houseNumber"
