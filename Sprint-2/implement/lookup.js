171635cfd7ed932fb51e8b8cf84e2818520bedb9
Trang <dothuytrang2312@gmail.com>
2026-01-18 16:42:11 +0100

    Complete Sprint-2/implement/lookup

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,9 @@
-function createLookup() {
-  // implementation here
+function createLookup(countryCurrencyPairs) {
+  let result = {}
+  for (i = 0; i < countryCurrencyPairs.length; i++) {
+    result[countryCurrencyPairs[i][0]] = countryCurrencyPairs[i][1]
+  }
+  return result
 }
 
 module.exports = createLookup;
--- Sprint-2/implement/lookup.test.js
+++ Sprint-2/implement/lookup.test.js
@@ -1,6 +1,11 @@
 const createLookup = require("./lookup.js");
 
-test.todo("creates a country currency code lookup for multiple codes");
+test("creates a country currency code lookup for multiple codes", () => {
+  expect(createLookup([['US', 'USD'], ['CA', 'CAD']])).toEqual({
+   'US': 'USD',
+   'CA': 'CAD'
+ })
+});
 
 /*
 
