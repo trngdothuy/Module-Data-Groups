f243e0100fcd33bfacefb20ea6c0d6bf23e732b3
Trang <dothuytrang2312@gmail.com>
2026-01-27 01:29:26 +0100

    Render list of books

--- Sprint-3/reading-list/script.js
+++ Sprint-3/reading-list/script.js
@@ -21,3 +21,22 @@ const books = [
   },
 ];
 
+// Render list of books: title, author and image and a background colour on whether we have read the book or not.
+const readingList = document.getElementById("reading-list")
+
+// loop through arr, each obj => add to readingItem
+books.forEach((e) => {
+  const readingItem = document.createElement("li")
+  readingList.appendChild(readingItem);
+
+  readingItem.innerHTML = `<div>
+  ${e.title} by ${e.author}
+  <img src=${e.bookCoverImage}>
+  </div>`
+
+  if (e.alreadyRead) {
+    readingItem.classList.add("green")
+  } else {
+    readingItem.classList.add("red")
+  }
+})
