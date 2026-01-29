614391282896f49abfe86fe6bcf567521ba9cf09
Trang <dothuytrang2312@gmail.com>
2026-01-29 14:21:25 +0100

    Active forward button

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -6,3 +6,17 @@ const images = [
 
 
 // Write your code here
+// TODO: Make forward, backward button active
+
+const backwardButton = document.getElementById("backward-btn");
+const carouselImg = document.getElementById("carousel-img")
+let currentPhotoIndex = 0;
+
+backwardButton.addEventListener("click", () => {
+    if (currentPhotoIndex === 0) {
+        currentPhotoIndex = images.length - 1;
+    } else {
+        currentPhotoIndex = currentPhotoIndex - 1;
+    }
+    carouselImg.src = images[currentPhotoIndex]
+})
