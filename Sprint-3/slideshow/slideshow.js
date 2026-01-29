f4169be8c627ee8f9692cd53ca213bd96b1856b6
Trang <dothuytrang2312@gmail.com>
2026-01-29 14:31:45 +0100

    Refactor reuse functions

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -6,23 +6,23 @@ const images = [
 
 
 // Write your code here
-// TODO: Make forward button active
+// TODO: Make auto-forward and auto-back buttons
 
 const backwardButton = document.getElementById("backward-btn");
 const forwardButton = document.getElementById("forward-btn");
 const carouselImg = document.getElementById("carousel-img")
 let currentPhotoIndex = 0;
 
-backwardButton.addEventListener("click", () => {
+function backward() {
     if (currentPhotoIndex === 0) {
         currentPhotoIndex = images.length - 1;
     } else {
         currentPhotoIndex = currentPhotoIndex - 1;
     }
     carouselImg.src = images[currentPhotoIndex]
-})
+}
 
-forwardButton.addEventListener("click", () => {
+function forward() {
     console.log(currentPhotoIndex)
     if (currentPhotoIndex === images.length - 1) {
         currentPhotoIndex = 0;
@@ -30,4 +30,8 @@ forwardButton.addEventListener("click", () => {
         currentPhotoIndex++;
     }
     carouselImg.src = images[currentPhotoIndex]
-})
+}
+
+backwardButton.addEventListener("click", backward)
+
+forwardButton.addEventListener("click", forward)
